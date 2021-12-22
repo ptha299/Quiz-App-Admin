e61ba6f8cf99a5f2d3de90cfcf41228c429539b9
unknown <anpi.2909@gmail.com>
2021-12-22 22:08:55 +0700

    Initial commit

--- src/routes/questions.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-
-// const questionController = require('../app/controllers/QuestionsController');
-
-// // router.get('/add', questionController.add);
-// router.get('/:slug', questionController.show);
-
-// module.exports = router;
